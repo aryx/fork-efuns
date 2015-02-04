6f27903a1d6702df12fb0237380810557177fe36
pad <pad@fb.com>
2015-02-04 09:33:41 -0800

    * graphics/xtypes.ml: so can compile! dumb values

---
+++ graphics/xK.ml
@@ -0,0 +1,58 @@
+
+let xk_Pointer_Button1 = 0
+let xk_Pointer_Button2 = 0
+let xk_Pointer_Button3 = 0
+let xk_Pointer_Button4 = 0
+let xk_Pointer_Button5 = 0
+
+let name_to_keysym = []
+let keysym_to_name = []
+
+let xk_Pointer_Drag1 = 1
+
+
+let xk_eacute = 0
+let xk_egrave = 0
+let xk_ccedilla = 0
+let xk_agrave = 0
+let xk_ugrave = 0
+let xk_mu = 0
+let xk_sterling = 0
+let xk_section = 0
+let xk_degree = 0
+
+let xk_Return = 0
+
+let xk_Up = 0
+let xk_Down = 0
+let xk_Left = 0
+let xk_Right = 0
+let xk_BackSpace = 0
+let xk_Delete = 0
+let xk_Tab = 0
+let xk_Prior = 0
+let xk_Next = 0
+
+let xk_grave  = 0
+let xk_apostrophe  = 0
+
+let xk_igrave  = 0
+let xk_ograve  = 0
+let xk_Agrave  = 0
+let xk_Egrave  = 0
+let xk_Igrave  = 0
+let xk_Ograve  = 0
+let xk_Ugrave  = 0
+let xk_Eacute  = 0
+let xk_aacute  = 0
+let xk_iacute  = 0
+let xk_oacute  = 0
+let xk_uacute  = 0
+let xk_Aacute  = 0
+let xk_Iacute = 0
+let xk_Oacute = 0
+let xk_Uacute = 0
+
+let xk_q = 0
+let xk_Insert = 0
+
