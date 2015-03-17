6b882c4667df481a87435d030972d84886e28391
pad <pad@fb.com>
2015-03-17 16:16:35 -0700

    * features/simple.ml: remove TODO

--- features/simple.ml
+++ features/simple.ml
@@ -724,36 +724,30 @@ let highlighted_chars = ref []
 
 (*s: function Simple.unhightlight *)
 let unhightlight () =
-    failwith "Simple.unhighlight: TODO"
-(*
-  List.iter (fun (buf,curseur,attr) ->
+  !highlighted_chars |> List.iter (fun (buf,curseur,attr) ->
       let text = buf.buf_text in
       set_char_attr text curseur attr;
       buf.buf_modified <- buf.buf_modified + 1;
       remove_point text curseur
-  ) !highlighted_chars;
+  );
   highlighted_chars := [];
-    match !highlighted with
-      None -> ()
-    | Some (frame,debut,fin) -> 
-        if !keypressed <> XK.xk_Pointer_Drag1 then
-          let top_window = Window.top frame.frm_window in
-          let xterm = Window.xterm top_window in
-          highlighted := None;
-          
-          let buf = frame.frm_buffer in
-          let text = buf.buf_text in
-          let curseur = add_point text in
-          let final = add_point text in
-          set_position text curseur debut;
-          set_position text final fin;
-          let str = Text.region text curseur final in
-          remove_point text curseur;
-          remove_point text final;
-          kill_string str;
-          WX_xterm.set_cutbuffer xterm str;
-          unhightlight_region buf debut fin
-          *)
+  match !highlighted with
+  | None -> ()
+  | Some (frame,debut,fin) -> 
+     (*   if !keypressed <> XK.xk_Pointer_Drag1 then *)
+     highlighted := None;
+     let buf = frame.frm_buffer in
+     let text = buf.buf_text in
+     let curseur = Text.add_point text in
+     let final = Text.add_point text in
+     Text.set_position text curseur debut;
+     Text.set_position text final fin;
+     let str = Text.region text curseur final in
+     Text.remove_point text curseur;
+     Text.remove_point text final;
+     kill_string str;
+     (* ??? WX_xterm.set_cutbuffer xterm str; for interop? *)
+     unhightlight_region buf debut fin
 (*e: function Simple.unhightlight *)
   
 (*s: function Simple.highlight *)
