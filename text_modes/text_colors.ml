0d996035dd830d16986a7177990ceafa48147e15
pad <pad@fb.com>
2015-03-19 10:17:22 -0700

    * text_modes/text_colors.ml: empty for now

---
+++ text_modes/text_colors.ml
@@ -0,0 +1,15 @@
+
+(*
+section_1
+section_2
+section_3
+section_4
+section_5
+
+(Set-face-foreground 'font-latex-sectioning-1-face "coral")
+(Set-face-foreground 'font-latex-sectioning-2-face "orange")
+(Set-face-foreground 'font-latex-sectioning-3-face "lime green")
+(Set-face-foreground 'font-latex-sectioning-4-face "lightblue3")
+(Set-face-foreground 'font-latex-sectioning-5-face "gray")
+
+*)
