be90c924f3ae18b12955e25bd0cff1f0da9f4495
pad <pad@fb.com>
2015-03-23 16:21:15 -0700

    * features/system.mli:

---
+++ features/system.mli
@@ -0,0 +1,9 @@
+
+val system : string -> string -> 'a -> Efuns.buffer
+val start_command : string -> Efuns.window -> string -> Efuns.frame
+val shell_command : Efuns.frame -> unit
+
+(*
+val shell_hist : string list ref
+val open_process : string -> int * in_channel * out_channel
+*)
