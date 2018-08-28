68c1338843d34831fd2db4e15d5247a646acf6fb
pad <yoann.padioleau@gmail.com>
2018-08-28 09:53:06 -0700

    * todo.txt: misc

--- pad.ml
+++ pad.ml
@@ -3,6 +3,7 @@ open Efuns
 (* 
  * For really pad-specific stuff.
  * Hopefully it will be shorter than my .emacs ...
+ * alt: have a .efunsrc, but not everything is customizable through options.
  *)
 
 let _ =
@@ -10,14 +11,14 @@ let _ =
 
     (* special functions *)
 
-    Keymap.define_interactive_action "gtd" (fun frame ->
-      Frame.load_file frame.frm_window "/home/pad/GTD/GTD-daily.org" |> ignore
+    Keymap.define_interactive_action "gtd" (fun frm ->
+      Frame.load_file frm.frm_window "/home/pad/GTD/GTD-daily.org" |> ignore
     );
 
     (* ~/.login like *)
     Unix.putenv "PFFF_HOME" "/home/pad/pfff";
     Unix.putenv "PATH" (
-       "/home/pad/.opam/4.01.0/bin:" ^
+       "/home/pad/.opam/4.02.3/bin:" ^
         (try (Unix.getenv "PATH") with Not_found -> "/bin") ^ 
        ":/home/pad/bin"
      );
