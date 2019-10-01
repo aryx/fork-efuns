04e4269984caa705133e568fbd3236cf6b5c3991
pad <yoann.padioleau@gmail.com>
2019-10-01 11:19:54 +0200

    new Python mode!

--- pfff_modes/python_mode.ml
+++ pfff_modes/python_mode.ml
@@ -12,7 +12,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
  * license.txt for more details.
  *)
-open Common
 
 open Efuns
 module PI = Parse_info
@@ -20,18 +19,14 @@ module PI = Parse_info
 (*****************************************************************************)
 (* Prelude *)
 (*****************************************************************************)
-(* Pfff-based Javascript major mode.
+(* Pfff-based Python major mode.
  * 
- * This module provides a major mode to edit Javascript files by using
- * the Javascript parser and highlighter in Pfff 
+ * This module provides a major mode to edit Python files by using
+ * the Python parser and highlighter in Pfff 
  * (which is also used for codemap).
- *
- * TODO It relies on ocp-indent and merlin for the rest.
  *
  * todo:
  *  - support Bento
- *  - Tab handling when in comment and indent and add '*' leading if needed
- *  - finish Merlin support (see ocaml_merlin.ml)
  *)
 
 (*****************************************************************************)
@@ -40,12 +35,12 @@ module PI = Parse_info
 let funcs = { Pfff_modes.
   parse = (fun file ->
     Common.save_excursion Flag_parsing.error_recovery true (fun()->
-      let (astopt, toks), _stat = Parse_js.parse file in
-      [astopt ||| [], toks]
+      let (astopt, toks), _stat = Parse_python.parse file in
+      [astopt, toks]
     )
   );
   highlight = (fun ~tag_hook prefs (ast, toks) -> 
-    Highlight_js.visit_program ~tag_hook prefs (ast, toks)
+    Highlight_python.visit_program ~tag_hook prefs (ast, toks)
   );
   }
 
@@ -72,19 +67,17 @@ let color_buffer buf =
 (*****************************************************************************)
 (* Indentation *)
 (*****************************************************************************)
-(* TODO See ocaml_ocp_indent.ml *)
 
 (*****************************************************************************)
 (* Completion/navigation/... *)
 (*****************************************************************************)
-(* TODO see ocaml_merlin.ml *)
 
 (*****************************************************************************)
 (* The mode *)
 (*****************************************************************************)
-let hooks = Store.create_abstr "javascript_mode_hook"
+let hooks = Store.create_abstr "python_mode_hook"
 
-let mode =  Ebuffer.new_major_mode "JS(Pfff)" (Some (fun buf ->
+let mode =  Ebuffer.new_major_mode "Python(Pfff)" (Some (fun buf ->
   color_buffer buf; 
 
   buf.buf_syntax_table.(Char.code '_') <- true;
@@ -99,7 +92,7 @@ let mode =  Ebuffer.new_major_mode "JS(Pfff)" (Some (fun buf ->
   Hook.exec_hooks hooks buf;
 ))
 
-let javascript_mode = 
+let python_mode = 
   Major_modes.enable_major_mode mode
 [@@interactive]
 
@@ -110,20 +103,8 @@ let javascript_mode =
 let _ =
   Hook.add_start_hook (fun () ->
     Var.add_global Ebuffer.modes_alist 
-      [".*\\.\\(js\\|ts\\)$",mode];
+      [".*\\.\\(py\\|py\\)$",mode];
     
-    (* reuse some functions from ocaml_mode.ml from LeFessant *)
-    (* 
-    Var.set_major_var mode Compil.find_error Ocaml_mode.find_error;
-    Var.set_major_var mode Compil.find_error_location_regexp 
-      (snd !!Ocaml_mode.error_regexp);
-    *)
-    (*
-    Var.set_major_var mode Indent.indent_func Ocaml_mode.indent_between_points;
-    Keymap.add_major_key mode [NormalMap,XK.xk_Tab] 
-     Ocaml_mode.indent_current_line;
-    *)
-
     (* recolor at save time *)
     Var.set_major_var mode Ebuffer.saved_buffer_hooks
       (color_buffer::(Var.get_global Ebuffer.saved_buffer_hooks));
